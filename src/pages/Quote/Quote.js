44fb0affe4cff7887338082c54910db569d3e81b
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-02-21 18:15:58 -0400

    add quote page

--- src/pages/Quote/Quote.js
+++ src/pages/Quote/Quote.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Screen } from '../../components';
 
+import styles from './Quote.module.css';
+
 function Quote() {
   return (
     <Screen>
-      <div>Quote</div>
+      <div className={styles.container}>
+        <h1>
+          Mathematics is not about numbers,
+          equations, computations, or algorithms: it is about understanding. _William Paul Thurston
+        </h1>
+      </div>
     </Screen>
   );
 }
