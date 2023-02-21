d896b4d052ae92f4ecf40709bd4d01608683fdac
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-02-21 15:04:55 -0400

    add browser navigation configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter, Routes, Route, Outlet,
+} from 'react-router-dom';
 import { Navbar, Calculator } from './components';
+import { Home, Quote } from './pages';
+
+const Layout = () => {
+  <>
+    <Navbar />
+    <Outlet />
+  </>;
+};
 
 const App = () => (
   <>
     <Navbar />
-    <Calculator />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </>
 );
 
