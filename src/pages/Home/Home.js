4cb8473a797eaf3f29cc5da834d983ffc33e5066
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-02-21 17:35:39 -0400

    add home page components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Screen } from '../../components';
 
+import styles from './Home.module.css';
+import Side from '../../assets/images/side2.svg';
+
 function Home() {
   return (
     <Screen>
-      <div>Home</div>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <div>
+            <h1>Ever wanted to do your math quicker?</h1>
+            <p>Stop searching for that today, your are at the right place! 😊</p>
+            <button type="button">Let`s go!</button>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <img className={styles.imgSide} src={Side} alt="Person reading" />
+        </div>
+      </div>
     </Screen>
   );
 }
